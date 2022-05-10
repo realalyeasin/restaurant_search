73aa399bdb505adaccdf24c051706db81f4a7eb0
Yeasin <realalyeasin@gmail.com>
2022-05-10 17:28:55 +0600

    api

--- lib/main.dart
+++ lib/main.dart
@@ -26,12 +26,10 @@ class Restaurant extends StatelessWidget {
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
 
-  final dio = Dio(
-    BaseOptions(baseUrl: 'https://api.spoonacular.com/recipes/716429/information?apiKey=179d4592a9354125ab7aa9ac3b8b3fb9&includeNutrition=true.',
-    headers: {
-      'apiKey' : '179d4592a9354125ab7aa9ac3b8b3fb9'
-    })
-  );
+  final dio = Dio(BaseOptions(
+      baseUrl:
+          'https://api.spoonacular.com/recipes/716429/information?apiKey=179d4592a9354125ab7aa9ac3b8b3fb9&includeNutrition=true.',
+      headers: {'apiKey': '179d4592a9354125ab7aa9ac3b8b3fb9'}));
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -41,20 +39,21 @@ class _HomePageState extends State<HomePage> {
 
   List? _lists;
   Future searchRestaurents() async {
-    final response = await widget.dio.get('https://api.spoonacular.com/recipes/716429/information?apiKey=179d4592a9354125ab7aa9ac3b8b3fb9&includeNutrition=true.',
-    queryParameters: {
-      'diet' : 'diet'
-    });
+    final response = await widget.dio.get(
+        'https://api.spoonacular.com/recipes/716429/information?apiKey=179d4592a9354125ab7aa9ac3b8b3fb9&includeNutrition=true.',
+        queryParameters: {'diet': 'diet'});
     setState(() {
       _lists = response.data['extendedIngredients'];
       debugPrint(_lists.toString());
     });
   }
+
   @override
   void initState() {
     searchRestaurents();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,46 +63,157 @@ class _HomePageState extends State<HomePage> {
           title: Text("Search Restaurant"),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-          Form(
-              key: _formKey,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.search),
-                    filled: true,
-                    border: OutlineInputBorder()),
-                    validator: (value){
-                      if(value!.isEmpty){
-                        return "Please enter a proper name";
-                      }
-                      return null;
-                    },
-              ),),
-          SizedBox(
-            width: double.infinity,
-            child: FlatButton(
-              color: Colors.yellow,
-              onPressed: (){
-                searchRestaurents();
-              },
-              focusColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8)
+            padding: const EdgeInsets.all(8.0),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Form(
+                key: _formKey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      filled: true,
+                      border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter a proper name";
+                    }
+                    return null;
+                  },
+                ),
               ),
-              child: Text("Search", style: TextStyle(color: Colors.black, letterSpacing: 1, fontWeight: FontWeight.bold),),
-            ),
-          ),
-            _lists !=null ? ListView.builder(
-                itemCount: _lists!.length,
-                shrinkWrap: true,
-                itemBuilder: (BuildContext, index){
-                  return Text(_lists![index]['id'].toString());
-                }) : Text('noo')
-        ]
-        )));
+              SizedBox(
+                width: double.infinity,
+                child: FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    searchRestaurents();
+                  },
+                  focusColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Text(
+                    "Search",
+                    style: TextStyle(
+                        color: Colors.black,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              _lists != null
+                  ? Expanded(
+                      child: ListView.separated(
+                        itemCount: _lists!.length,
+                        shrinkWrap: true,
+                        itemBuilder: (BuildContext, index) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.orangeAccent,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "ID - "
+                                    '${_lists![index]['id'].toString()}',
+                                    style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Aisle - "
+                                    '${_lists![index]['aisle'].toString()}',
+                                    style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Divider(
+                                    height: 2,
+                                    thickness: 2,
+                                    color: Colors.blue,
+                                  ),
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Text(
+                                    "Consistency - "
+                                    '${_lists![index]['consistency'].toString()}',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Name -"
+                                    '${_lists![index]['name'].toString()}',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Name Clean - "
+                                    '${_lists![index]['nameClean'].toString()}',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Original - "
+                                    '${_lists![index]['original'].toString()}',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Original Name  "
+                                    '${_lists![index]['originalName'].toString()}',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Amount - "
+                                    '${_lists![index]['amount'].toString()}',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Unit - "
+                                    '${_lists![index]['unit'].toString()}',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Measures(US) - "
+                                    '${_lists![index]['measures']['us']['amount'].toString()}',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return SizedBox(
+                            child: Divider(
+                              color: Colors.black,
+                              height: 1,
+                            ),
+                            height: 15,
+                          );
+                        },
+                      ),
+                    )
+                  : Text('noo')
+            ])));
   }
 }
